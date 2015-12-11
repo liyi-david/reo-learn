b675c425d9853d24307c649bbad27a9410c40987
Liyi <liyi.pku@outlook.com>
2015-12-12 00:24:59 +0800

    x=.

--- example/fifo/oracle.go
+++ example/fifo/oracle.go
@@ -7,7 +7,7 @@ import "time"
 func GetOracle() *sul.Oracle {
 	o := new(sul.Oracle)
 	o.InPorts = []string{"A"}
-	o.MidPorts = []string{"M0"}
+	o.MidPorts = []string{"M0", "M1"}
 	o.OutPorts = []string{"B"}
 	o.TimeUnit = 100 * time.Millisecond
 	o.GenerateInst = func() *sul.SulInst {
@@ -17,10 +17,11 @@ func GetOracle() *sul.Oracle {
 		// we use one stop flag to close all of them
 		// and multiple stop finish flag to make sure that all of them
 		// are closed
-		r.StopPorts = reo.GenerateStopPort(2)
+		r.StopPorts = reo.GenerateStopPort(3)
 		r.Start = func() {
-			go reo.FifoChannel(r.InPorts["A"], r.MidPorts["M0"], r.StopPorts[0])
-			go reo.OutputChannel(r.MidPorts["M0"], r.OutPorts["B"], r.StopPorts[1])
+			go reo.FifoChannel(r.MidPorts["M0"], r.MidPorts["M1"], r.StopPorts[0])
+			go reo.OutputChannel(r.MidPorts["M1"], r.OutPorts["B"], r.StopPorts[1])
+			go reo.BufferChannel(10, r.InPorts["A"], r.MidPorts["M0"], r.StopPorts[2])
 		}
 		return r
 	}
