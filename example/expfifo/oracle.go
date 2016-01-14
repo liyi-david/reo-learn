31d9d00ef1f3c5fb3d34b701d67484fa71a3e234
Liyi <liyi.pku@outlook.com>
2016-01-14 15:53:25 +0800

    .

--- example/expfifo/oracle.go
+++ example/expfifo/oracle.go
@@ -4,15 +4,17 @@ import "../../lib/sul"
 import "../../lib/reo"
 import "time"
 import "strconv"
+import "fmt"
 
 func GetOracle() *sul.Oracle {
 	o := new(sul.Oracle)
 	o.InPorts = []string{"A"}
 	// dynamically generate the MidPorts
 	o.MidPorts = []string{}
-	for i := 0; i < 14; i++ {
+	for i := 0; i < 11; i++ {
 		o.MidPorts = append(o.MidPorts, "M"+strconv.Itoa(i))
 	}
+	fmt.Println(o.MidPorts)
 
 	o.OutPorts = []string{"B"}
 	o.TimeUnit = 40 * time.Millisecond
@@ -24,23 +26,20 @@ func GetOracle() *sul.Oracle {
 		// are closed
 		r.GeneratePort(o)
 		// generating stop ports
-		r.StopPorts = reo.GenerateStopPort(7)
+		r.StopPorts = reo.GenerateStopPort(10)
 		r.Start = func() {
 			// Input & Output
 			go reo.BufferChannel(1, r.InPorts["A"], r.MidPorts["M0"], r.StopPorts[0])
-			go reo.OutputChannel(r.MidPorts["M12"], r.OutPorts["B"], r.StopPorts[1])
+			go reo.OutputChannel(r.MidPorts["M10"], r.OutPorts["B"], r.StopPorts[1])
 			// Function Channels
-			go reo.SyncChannel(r.MidPorts["M0"], r.MidPorts["M1"], r.StopPorts[2])
-			go reo.ReplicatorChannel(r.MidPorts["M1"], r.MidPorts["M2"], r.MidPorts["M3"], r.StopPorts[3])
-			go reo.FifoChannel(r.MidPorts["M2"], r.MidPorts["M4"], r.StopPorts[4])
-			go reo.TimerChannel(r.MidPorts["M3"], r.MidPorts["M5"], 40*time.Millisecond, r.StopPorts[5])
-			go reo.LossysyncChannel(r.MidPorts["M5"], r.MidPorts["M6"], r.StopPorts[6])
-			go reo.ReplicatorChannel(r.MidPorts["M4"], r.MidPorts["M7"], r.MidPorts["M8"], r.StopPorts[7])
-			go reo.ReplicatorChannel(r.MidPorts["M6"], r.MidPorts["M9"], r.MidPorts["M10"], r.StopPorts[8])
-			go reo.SyncdrainChannel(r.MidPorts["M8"], r.MidPorts["M9"], r.StopPorts[9])
-			go reo.LossysyncChannel(r.MidPorts["M7"], r.MidPorts["M11"], r.StopPorts[10])
-			go reo.SyncChannel(r.MidPorts["M13"], r.MidPorts["M10"], r.StopPorts[11])
-			go reo.ReplicatorChannel(r.MidPorts["M11"], r.MidPorts["M12"], r.MidPorts["M13"], r.StopPorts[12])
+			go reo.ReplicatorChannel(r.MidPorts["M0"], r.MidPorts["M1"], r.MidPorts["M2"], r.StopPorts[2])
+			go reo.FifoChannel(r.MidPorts["M1"], r.MidPorts["M3"], r.StopPorts[3])
+			go reo.TimerChannel(r.MidPorts["M2"], r.MidPorts["M4"], 80*time.Millisecond, r.StopPorts[4])
+			go reo.LossysyncChannel(r.MidPorts["M4"], r.MidPorts["M5"], r.StopPorts[5])
+			go reo.ReplicatorChannel(r.MidPorts["M3"], r.MidPorts["M6"], r.MidPorts["M7"], r.StopPorts[6])
+			go reo.ReplicatorChannel(r.MidPorts["M5"], r.MidPorts["M8"], r.MidPorts["M9"], r.StopPorts[7])
+			go reo.SyncdrainChannel(r.MidPorts["M7"], r.MidPorts["M8"], r.StopPorts[8])
+			go reo.ReplicatorChannel(r.MidPorts["M6"], r.MidPorts["M10"], r.MidPorts["M9"], r.StopPorts[9])
 		}
 		return r
 	}
